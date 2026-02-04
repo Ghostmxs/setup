f116ca68f793319c0470704608d765f58da63c77
coder <kirill@khanin.pro>
2026-02-04 10:14:39 +0300

    fix: correct repo URL (setup, not redash-setup) + IPv4

--- setup.sh
+++ setup.sh
@@ -263,7 +263,7 @@ setup_compose() {
 	if [ "x$OVERWRITE" = "xyes" -a -e compose.yaml ]; then
 		mv -f compose.yaml compose.yaml.old-${TIMESTAMP_NOW}
 	fi
-	curl -fsSOL https://raw.githubusercontent.com/Ghostmxs/redash-setup/"$GIT_BRANCH"/data/compose.yaml
+	curl -4 -fsSOL https://raw.githubusercontent.com/Ghostmxs/setup/"$GIT_BRANCH"/data/compose.yaml
 
 	# Check for conflicts between --version and --preview options
 	if [ "x$PREVIEW" = "xyes" ] && [ -n "$REDASH_VERSION" ]; then
@@ -301,7 +301,7 @@ setup_compose() {
 create_make_default() {
 	echo "** Creating redash_make_default.sh script **"
 
-	curl -fsSOL https://raw.githubusercontent.com/Ghostmxs/redash-setup/"$GIT_BRANCH"/redash_make_default.sh
+	curl -4 -fsSOL https://raw.githubusercontent.com/Ghostmxs/setup/"$GIT_BRANCH"/redash_make_default.sh
 	sed -i "s|__COMPOSE_FILE__|$COMPOSE_FILE|" redash_make_default.sh
 	sed -i "s|__TARGET_FILE__|$PROFILE|" redash_make_default.sh
 	chmod +x redash_make_default.sh
